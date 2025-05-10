bce716be017c2af231ef9fd61caffa03755a0e58
2old4chuckEcheese <128924900+2old4chuckEcheese@users.noreply.github.com>
2025-05-10 16:39:08 -0400

    Create _app.js

---
+++ pages/_app.js
@@ -0,0 +1,23 @@
+import '@/styles/globals.css';
+import { WagmiConfig, createConfig, configureChains } from 'wagmi';
+import { mainnet } from 'wagmi/chains';
+import { publicProvider } from 'wagmi/providers/public';
+import { InjectedConnector } from 'wagmi/connectors/injected';
+
+const { publicClient } = configureChains([mainnet], [publicProvider()]);
+
+const config = createConfig({
+  autoConnect: true,
+  publicClient,
+  connectors: [
+    new InjectedConnector()
+  ]
+});
+
+export default function App({ Component, pageProps }) {
+  return (
+    <WagmiConfig config={config}>
+      <Component {...pageProps} />
+    </WagmiConfig>
+  );
+}
