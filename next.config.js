9b2e33393ab24300f13f2f1247674aaa3ccf7b81
2old4chuckEcheese <128924900+2old4chuckEcheese@users.noreply.github.com>
2025-05-10 18:24:17 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      process.env.CLOUDINARY_CLOUD_NAME + ".res.cloudinary.com",
+      "img.youtube.com",
+      "pbs.twimg.com"
+    ]
+  }
 };
-
-module.exports = nextConfig;
