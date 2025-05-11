ff866367770ada749903ae5e11effbe4c2fd8ffc
2old4chuckEcheese <128924900+2old4chuckEcheese@users.noreply.github.com>
2025-05-10 20:56:51 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,18 +1,24 @@
+// next.config.js
+
+// 1. Import and configure next-pwa
 const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
   skipWaiting: true,
 });
-module.exports = withPWA({
-  /** @type {import('next').NextConfig} */
-module.exports = {
+
+// 2. Define your Next.js config object
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: [
-      process.env.CLOUDINARY_CLOUD_NAME + ".res.cloudinary.com",
+      `${process.env.CLOUDINARY_CLOUD_NAME}.res.cloudinary.com`,
       "img.youtube.com",
-      "pbs.twimg.com"
-    ]
-  }
+      "pbs.twimg.com",
+    ],
+  },
 };
-});
+
+// 3. Wrap and export withPWA
+module.exports = withPWA(nextConfig);
