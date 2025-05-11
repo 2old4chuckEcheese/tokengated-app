36ee77834590f9c6693df223cb3eb928e8ff0b15
2old4chuckEcheese <128924900+2old4chuckEcheese@users.noreply.github.com>
2025-05-10 22:20:01 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,33 +1,9 @@
 // pages/_app.js
 //import '../styles/globals.css' // revisit tailwinds
-import { WagmiConfig, createConfig, configureChains } from 'wagmi'
-import { mainnet } from 'wagmi/chains'
-import { createPublicClient, http } from 'viem'
-import { InjectedConnector } from 'wagmi/connectors'
-
-// 1. Configure chains with a Viem-backed public client
-const { chains, publicClient, webSocketPublicClient } = configureChains(
-  [mainnet],
-  [
-    ({ chain }) =>
-      createPublicClient({
-        chain,
-        transport: http(),
-      }),
-  ]
-)
-
-// 2. Build your Wagmi config
-const wagmiConfig = createConfig({
-  autoConnect: true,
-  connectors: [
-    new InjectedConnector({
-      chains,       // <-- make sure this matches the destructured 'chains' above
-    }),
-  ],
-  publicClient,        // <-- comma here is required
-  webSocketPublicClient
-})
+// pages/_app.js
+import { WagmiConfig } from 'wagmi'
+import { wagmiConfig } from '../lib/wagmi'
+import '../styles/globals.css'
 
 export default function MyApp({ Component, pageProps }) {
   return (
