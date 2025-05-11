4c18c614d4ef9b8bf14660a687e7c5a017fa63a9
2old4chuckEcheese <128924900+2old4chuckEcheese@users.noreply.github.com>
2025-05-10 22:25:52 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,4 @@
-// pages/_app.js
 //import '../styles/globals.css' // revisit tailwinds
-// pages/_app.js
 import { WagmiConfig } from 'wagmi'
 import { wagmiConfig } from '../lib/wagmi'
 import '../styles/globals.css'
